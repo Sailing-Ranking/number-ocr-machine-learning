e8af4712c8e6c342a39728d74a0ec409f3edbb51
michael-gvdw <michael@fragrant-chios.com>
2022-12-01 17:02:46 +0100

    styling

--- src/01_train.py
+++ src/01_train.py
@@ -1,14 +1,6 @@
 import json
 
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-from sklearn.metrics import (
-    accuracy_score,
-    balanced_accuracy_score,
-    precision_score,
-    recall_score,
-)
 
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 
